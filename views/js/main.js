173da8306ffef53cb2af1f8abb5da456577798b8
LalitKishor <lalitsinghbanna05@gmail.com>
2016-10-16 16:01:55 +0530

    again optimize for loop

--- views/js/main.js
+++ views/js/main.js
@@ -527,12 +527,12 @@ window.addEventListener('scroll', function () {
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+  var elem = document.createElement('img');
+  elem.className = 'mover';
+  elem.src = "images/pizza.png";
+  elem.style.height = "100px";
+  elem.style.width = "73.333px";
   for (var i = 0; i < 31; i++) {
-    var elem = document.createElement('img');
-    elem.className = 'mover';
-    elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
